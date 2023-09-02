f4e17aeeac7f682dfc585945bb1ad7f10f5a2446
Hari264 <133808897+Hari264@users.noreply.github.com>
2023-09-02 23:53:53 +0530

    Update SolidityError.sol

--- SolidityError.sol
+++ SolidityError.sol
@@ -1,25 +1,48 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
-contract Error {
-    function testRequire(uint _i) public pure {
- 
-        require(_i >= 100 && _i <= 1000 , "Input must be between 100 to 1000 msg by requirea");
+contract ComplexErrorDemonstration {
+    address public owner;
+    uint256 public balance;
+    mapping(address => uint256) public userBalances;
+
+    event Deposit(address indexed user, uint256 amount);
+    event Withdrawal(address indexed user, uint256 amount);
+
+    constructor() {
+        owner = msg.sender;
     }
 
-    function testRevert(uint _i) public pure {
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function.");
+        _;
+    }
 
-        if (_i <= 100 && _i>=1000) {
-            revert("Input must be greater than 100 and lesser than 1000 msg by revert");
+    function deposit(uint256 amount) public {
+        require(amount > 0, "Deposit amount must be greater than zero.");
+        userBalances[msg.sender] += amount;
+        balance += amount;
+        emit Deposit(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount) public {
+        require(amount > 0, "Withdrawal amount must be greater than zero.");
+        require(userBalances[msg.sender] >= amount, "Insufficient balance.");
+        userBalances[msg.sender] -= amount;
+        balance -= amount;
+        emit Withdrawal(msg.sender, amount);
+    }
+
+
+    function triggerRevert() public pure {
+        if (true) {
+            revert("This function intentionally triggers a revert error.");
         }
     }
 
-    uint public num;
-//check unvarients
-    function testAssert() public view {
-       
-        assert(num != 0);
+    function triggerAssert() public pure {
+        uint256 a = 1;
+        uint256 b = 2;
+        assert(a > b); // Triggers an assert error because the condition is not met.
     }
-
-   
 }
